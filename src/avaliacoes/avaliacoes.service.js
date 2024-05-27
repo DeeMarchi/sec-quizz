58f0bb1f293d506d7e1cb6dda47522e2be67d539
Denner Marchi <denner883@hotmail.com>
2024-05-27 20:13:04 -0300

    Refactor, movendo a lógica de tratamento do body para as perguntas

--- src/avaliacoes/avaliacoes.service.js
+++ src/avaliacoes/avaliacoes.service.js
@@ -2,21 +2,12 @@ const { Avaliacao, sequelize } = require('../db/models');
 
 const PerguntaService = require('../perguntas/perguntas.service');
 
-const converterStringParaBool = string => string === 'true';
-
 module.exports = {
     create: (idUsuario, nota) => Avaliacao.create({ idUsuario, nota }),
 
     calcularNota: async body => {
-        const respostasConvertidas = (await PerguntaService.findAll())
-            .map(pergunta => pergunta.id)
-            .map(id => {
-                return {
-                    id,
-                    respostaPergunta: converterStringParaBool(body['pergunta' + id])
-                }
-            });
-        const respostasPositivas = respostasConvertidas.filter(resposta => resposta.respostaPergunta);
+        const respostasConvertidas = await PerguntaService.converterParaRespostas(body);
+        const respostasPositivas = PerguntaService.filtrarRespostasPositivas(respostasConvertidas);
         const totalPerguntasSistema = respostasConvertidas.length;
         const totalPositivas = respostasPositivas.length;
         const nota = (totalPositivas / totalPerguntasSistema) * 10;
--- src/perguntas/perguntas.service.js
+++ src/perguntas/perguntas.service.js
@@ -1,9 +1,30 @@
 const { Pergunta } = require('../db/models');
 
-module.exports = {
-    findAll() {
-        return Pergunta.findAll({
-            attributes: [ 'id', 'texto' ]
+const converterStringParaBool = string => string === 'true';
+
+const findAll = () => {
+    return Pergunta.findAll({
+        attributes: [ 'id', 'texto' ]
+    });
+};
+
+const converterParaRespostas = async body => {
+    return (await findAll())
+        .map(pergunta => pergunta.id)
+        .map(id => {
+            return {
+                id,
+                respostaPergunta: converterStringParaBool(body['pergunta' + id])
+            }
         });
-    }
+};
+
+const filtrarRespostasPositivas = perguntasConvertidas => {
+    return perguntasConvertidas.filter(pergunta => pergunta.respostaPergunta);
+};
+
+module.exports = {
+    findAll,
+    converterParaRespostas,
+    filtrarRespostasPositivas
 };
